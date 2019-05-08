2b5f3260e13a1cf6d04a4a9a6362292c74dcfcf5
chenguangci <1771382149@qq.com>
2019-05-08 18:23:33 +0800

    第一个Python web程序

---
+++ www/app.py
@@ -0,0 +1,18 @@
+import asyncio, os, json, time
+import logging; logging.basicConfig(level=logging.INFO)
+from datetime import datetime
+from aiohttp import web
+
+
+async def index():
+    return web.Response(body='<h1>Hello BeiYi</h1>'.encode('utf-8'), content_type='text/html')
+
+
+def init():
+    app = web.Application()
+    app.router.add_routes([web.get('/', index)])
+    logging.info('Server started at http://127.0.0.1:8080')
+    web.run_app(app, host='127.0.0.1', port=8080)
+
+
+init()
